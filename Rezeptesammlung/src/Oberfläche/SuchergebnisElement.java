1d31f55839f25a349be0e08731d392d25ab3399c
itdlz-mast <itdlz-mast@ITDLZ-NB-0206.finanzen.bayern.de>
2015-11-17 10:41:37 +0100

    Hinzufügen SuchergebnisElement von Merlin und Bianca

---
+++ Rezeptesammlung/src/Oberfläche/SuchergebnisElement.java
@@ -0,0 +1,82 @@
+package Oberfläche;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+
+public class SuchergebnisElement extends HBox {
+
+	private ImageView vorschaubild;
+	private String arbeitszeit;
+	private String kochbackzeit;
+	private String schwierigkeit;
+	private String kalorienpp;
+	private Label text;
+
+	
+	/* Konstruktor der Klasse, der falls kein Bild verfügbar ist, ein Standardbild anzeigt
+	 * 
+	 */
+	public SuchergebnisElement(String bildurl, String beschreibung) {
+		try{
+			Image thumbnail = new Image(bildurl);
+
+			this.vorschaubild = new ImageView(thumbnail);
+			vorschaubild.setFitWidth(200);
+			vorschaubild.setPreserveRatio(true);
+			
+//			this.vorschaubild = new ImageView(new Image(bildurl, 220, 165, true, true));
+		}catch(Exception e){
+			System.out.println("kein bild");
+			this.vorschaubild = new ImageView(new Image("http://img1.wikia.nocookie.net/__cb20121110160830/vampirediaries/de/images/4/48/Kein_bild.jpg"));
+		}
+		
+		this.getChildren().addAll(vorschaubild, splitText(beschreibung));
+	}
+
+	
+	
+	/* Methode die den String von Arbeitszeit, Koch/Backzeit, Schwierigkeit und Kalorien p.P.
+	 * in die jeweiligen Elemente zerlegt, immer bei " / " wird getrennt.
+	 * Die Elemente des Arrays, welches durch split() entsteht, werden in einzelne String-Variablen
+	 * gespeichert.
+	 */
+	private Label splitText(String orgText){
+		
+		try{String[] array = orgText.split(" / ");
+		
+			arbeitszeit = array[0];
+			kochbackzeit = array[1];
+			schwierigkeit = array[2];
+			kalorienpp = array[3];
+
+		}catch(Exception e){
+			System.out.println("nix zum zeilen umbrechen gefunden");
+		}
+	
+		this.text = new Label(arbeitszeit + "\n" + kochbackzeit + "\n" + schwierigkeit + "\n" + kalorienpp);
+		text.setFont(new Font("Sylfaen",14));
+		
+		return text;
+	}
+	
+	
+	/* Eine Methode die mittels PixelReader() und WritableImage() ein übergebenes Bild beschneidet und
+	 * in den Ausschnitt mittig auf das Bild anwendet.
+	 */
+//	private Image cropImage(Image orgPic){
+//		
+//		int widthhalf = (int)(orgPic.getWidth()/2) - 110;
+//		int heighthalf = (int)(orgPic.getHeight()/2) - 83;
+//		
+//		PixelReader reader = orgPic.getPixelReader();
+//		WritableImage croppedImage = new WritableImage(reader, widthhalf, heighthalf, 220, 165);
+//		
+//		return croppedImage;
+//	}
+	
+}
