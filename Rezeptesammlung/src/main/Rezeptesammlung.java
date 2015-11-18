27a25bafacff7447395c9ce92ddbf58b21ab03bb
itdlz-mast <itdlz-mast@ITDLZ-NB-0206.finanzen.bayern.de>
2015-11-18 12:46:26 +0100

    prodIndex aktiv

--- Rezeptesammlung/src/main/Rezeptesammlung.java
+++ Rezeptesammlung/src/main/Rezeptesammlung.java
@@ -52,9 +52,9 @@ public class Rezeptesammlung {
 
 			}
 		}
-		// Methoden.deleteDir(file);
+		Methoden.deleteDir(file);
 		writer.close();	
-		such.suchen("und");
+		//such.suchen("und");
 		
 	}
 
