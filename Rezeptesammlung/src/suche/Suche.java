64b6431c735c211143fa28331792de058d666bf1
vi-bbria <vi-bbria@fa-ls104-09.hof-university.de>
2015-11-25 12:30:46 +0100

    formatiert oder so

--- Rezeptesammlung/src/suche/Suche.java
+++ Rezeptesammlung/src/suche/Suche.java
@@ -1,9 +1,8 @@
 package suche;
 
+//hier
 import java.io.IOException;
-
 import objekte.Suchobjekt;
-
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.queryparser.classic.ParseException;
@@ -15,26 +14,28 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.util.Version;
 import main.Rezeptesammlung;
 
-public class Suche 
-{
-	public Suchobjekt[] suchen(String suchbegriff) throws IOException, ParseException
-	{
+public class Suche {
+	
+	public Suchobjekt[] suchen(String suchbegriff) throws IOException, ParseException {
+		
 		DirectoryReader dr = DirectoryReader.open(Rezeptesammlung.indexDir);
 		IndexSearcher searcher = new IndexSearcher(dr);
 		QueryParser qp = new QueryParser(Version.LUCENE_45, "Inhalt", Rezeptesammlung.analyzer);
-		Query query = qp.parse(suchbegriff );
-
-
+		Query query = qp.parse(suchbegriff);
 		TopDocs td = searcher.search(query, 10);
 		ScoreDoc[] sd = td.scoreDocs;
 		Suchobjekt[] ergebnisObjekt = new Suchobjekt[sd.length];
+		
 		for (int i = 0; i < sd.length; i++) {
 			Document doc = searcher.doc(sd[i].doc);
 			System.out.println(doc.get("Beschreibung"));
-			ergebnisObjekt[i] = new Suchobjekt(doc.get("Titel"), doc.get("Bild"), doc.get("Beschreibung"), doc.get("PubDate"), doc.get("Inhalt"), doc.get("Link"));
+			ergebnisObjekt[i] = new Suchobjekt(doc.get("Titel"),
+					doc.get("Bild"), doc.get("Beschreibung"),
+					doc.get("PubDate"), doc.get("Inhalt"), doc.get("Link"));
 		}
+		
 		dr.close();
 		return ergebnisObjekt;
+		
 	}
-
 }
