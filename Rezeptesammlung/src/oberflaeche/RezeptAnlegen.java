c21869a62128c59c35fae1b5380eb76ed2948cab
freakadings <freakadings@Merlin.fritz.box>
2016-01-20 01:29:20 +0100

    Doku "fertig"

--- Rezeptesammlung/src/oberflaeche/RezeptAnlegen.java
+++ Rezeptesammlung/src/oberflaeche/RezeptAnlegen.java
@@ -1,7 +1,12 @@
 package oberflaeche;
 
+import java.io.IOException;
+
+import einlesen.Rezepte;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -10,49 +15,48 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-import java.io.IOException;
-
-import einlesen.Rezepte;
-
 public class RezeptAnlegen extends VBox {
 
 	public RezeptAnlegen() {
 		TextField titel = new TextField();
-		titel.setPromptText("Titel");
+			titel.setPromptText("Titel");
 		TextArea beschreibung = new TextArea();
-		beschreibung.setPromptText("Beschreibung");
+			beschreibung.setPromptText("Beschreibung");
 		TextField tags = new TextField();
-		tags.setPromptText("Tags durch Komma getrennt einegben");
-		Label azeit = new Label("Arbeitszeit:");
-		Label kbzeit = new Label("Koch-/Backzeit:");
-		Label schwierig = new Label("Schwierigkeit:");
-		Label kalor = new Label("Kalorien:");
+			tags.setPromptText("Tags durch Komma getrennt einegben");
+		Label arbeitszeitLabel = new Label("Arbeitszeit:");
+		Label kochUndBackzeitLabel = new Label("Koch-/Backzeit:");
+		Label schwierigkeitLabel = new Label("Schwierigkeit:");
+		Label kalorienLabel = new Label("Kalorien:");
 		TextField arbeitszeit = new TextField("Keine Angabe");
 		TextField kochBackZeit = new TextField("Keine Angabe");
 		ComboBox schwierigkeit = new ComboBox();
-		schwierigkeit.getItems().addAll("einfach", "normal", "schwierig");
-		schwierigkeit.setValue("normal");
+			schwierigkeit.getItems().addAll("einfach", "normal", "schwierig");
+			schwierigkeit.setValue("normal");
+			
 		TextField kalorien = new TextField("Keine Angabe");
-		Button speichern = new Button("speichern");
-		speichern.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent event) {
-				System.out.println(titel.getText() + " " + beschreibung.getText() + " " + arbeitszeit.getText() + " "
-						+ kochBackZeit.getText() + " " + schwierigkeit.getValue().toString() + " " + kalorien.getText()
-						+ " " + tags.getText());
-				Rezepte neuesRezept = new Rezepte();
-				try {
-					neuesRezept.neuesRezeptEinfügen(titel.getText(),beschreibung.getText(),arbeitszeit.getText(),kochBackZeit.getText(),schwierigkeit.getValue().toString(),kalorien.getText(),tags.getText());
-				} catch (IOException e) {
-					System.out.println("Fehler beim Einlesen!\n" + e.toString());
+		
+		Button speichern = new Button("Speichern");
+			speichern.setOnAction(new EventHandler<ActionEvent>() {
+				public void handle(ActionEvent event) {
+
+					Rezepte neuesRezept = new Rezepte();
+					try {
+						neuesRezept.neuesRezeptEinfügen(titel.getText(),beschreibung.getText(),arbeitszeit.getText(),kochBackZeit.getText(),schwierigkeit.getValue().toString(),kalorien.getText(),tags.getText());
+					} catch (IOException e) {
+						System.out.println("Fehler beim Einlesen!\n" + e.toString());
+					}
+					event.consume();
 				}
-				event.consume();
-			}
-		});
-		Button abbrechen = new Button("abbrechen");
+			});
+		
+		Button abbrechen = new Button("Abbrechen");
 
 		HBox buttonGruppe = new HBox();
-		buttonGruppe.getChildren().addAll(azeit, arbeitszeit, kbzeit, kochBackZeit, schwierig, schwierigkeit, kalor,
-				kalorien, speichern, abbrechen);
+			buttonGruppe.getChildren().addAll(arbeitszeitLabel, arbeitszeit, kochUndBackzeitLabel, kochBackZeit, schwierigkeitLabel, schwierigkeit, kalorienLabel, kalorien, speichern, abbrechen);
+			buttonGruppe.setSpacing(5);
+			buttonGruppe.setPadding(new Insets(5, 5, 5, 5));
+			buttonGruppe.alignmentProperty().set(Pos.CENTER);
 		this.getChildren().addAll(titel, beschreibung, tags, buttonGruppe);
 
 	}
